76bf971a33cb01158c1d85023ee8011c8ac61ebf
Thomas Vendetta <thomas@thomasvendetta.com>
2014-12-02 14:14:33 -0500

    indent guides

--- setup.sh
+++ setup.sh
@@ -1,4 +1,33 @@
-echo "Installing apt requisites and then installing  youcompleteme.vim"
-sudo apt-get install build-essential cmake python-dev
+#! /bin/bash
+
+if [[ "$OSTYPE" == "linux-gnu" ]]; then
+  echo "On Linux" 
+  echo "Installing apt requisites and then installing  youcompleteme.vim"
+  sudo apt-get install build-essential cmake python-dev
+elif [[ "$OSTYPE" == "darwin"* ]]; then
+  echo "On Mac"
+  brew install cmake
+fi
+
+# Grab vim bundles
 git submodule update --init --recursive
-./bundle/youcompleteme/install.sh --clang-completer
+
+# Install youcompleteme ctags
+#./bundle/youcompleteme/install.sh --clang-completer
+
+# Link git repo to vim stuffs
+if [ -e ~/.vimrc ]; then
+  echo "VimRC already exists. Backing it up to ~/.vimrc.backup so we don't overwite."
+  mv ~/.vimrc ~/.vimrc.backup
+fi
+
+ln -s $(pwd)/vimrc ~/.vimrc
+echo "Symlinked vimrc from $(pwd)/vimrc to ~/.vimrc"
+
+if [ -d ~/.vim ]; then
+  echo "Vim directory already exists. Backing it up to ~/.vim.backup"
+  mv ~/.vim ~/.vim.backup
+fi
+
+ln -s $(pwd) ~/.vim
+echo "Symlinked vim directory from $(pwd) to ~/.vim"
--- vimrc
+++ vimrc
@@ -1,3 +1,23 @@
+
+" Enable pathogen
 execute pathogen#infect()
-syntax on
-filetype plugin indent on
+
+set nocompatible      " We're running Vim, not Vi!
+syntax on             " Enable syntax highlighting
+filetype on           " Enable filetype detection
+filetype indent on    " Enable filetype-specific indenting
+filetype plugin on    " Enable filetype-specific plugins
+
+
+set expandtab
+set tabstop=2 shiftwidth=2 softtabstop=2
+set autoindent
+
+" Enable ctrlp.vim
+set runtimepath^=~/.vim/bundle/ctrlp.vim
+
+
+" Enable mouse use in all modes
+set ttyfast
+set mouse=a
+  
